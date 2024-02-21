1fc1c7ba462c59643ffac5c87cf48c68e5d9cadd
RoJack Luo <luo.zho@northeastern.com>
2024-02-21 18:37:21 -0500

    adoped multiple child processes

--- Week6/src/main.c
+++ Week6/src/main.c
@@ -3,15 +3,20 @@
 #endif
 
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <time.h>
 
 #include "genWord.h"
 #include "polybius.h"
 #include "queue.h"
 
+#define BATCH_SIZE 100
+
+/*
+ * Pipe the cipher to a file
+ */
 void writeCipherToFile(const char *sentence, table_t *table) {
   // Create a file name with the current time
   char fileName[256];
@@ -55,17 +60,16 @@ void writeCipherToFile(const char *sentence, table_t *table) {
   close(stdout_copy);
 }
 
-
 int main(int argc, char *argv[]) {
   /*
   1. Write 10,000 words into tmp.txt.
   */
   printf("Generating random words...\n");
-  #if TEST
-    int num = 150;
-  #else
-    int num = 10000;
-  #endif
+#if TEST
+  int num = 150;
+#else
+  int num = 10000;
+#endif
 
   char *tmpTxt = "tmp.txt";
   FILE *wirte = fopen(tmpTxt, "w");
@@ -78,24 +82,24 @@ int main(int argc, char *argv[]) {
     char *r = genWord();
     fprintf(wirte, "%s", r);
     if (i != num - 1) fprintf(wirte, "\n");
-    #if TEST
-      printf("%d Writing: %s into %s\n", i + 1, r, tmpTxt);
-    #endif
+#if TEST
+    printf("%d Writing: %s into %s\n", i + 1, r, tmpTxt);
+#endif
     free(r);
   }
 
   fclose(wirte);
 
-  #if TEST
+#if TEST
   printf("==========Test1==========\n");
-  int after = lineCnt(tmpTxt) + 1; // get the line number after write
-  if(num == after){
-      printf("Successfully wrote %d lines into %s.\n", num, tmpTxt);
-  }else{
-      printf("Failed: num = %d, after = %d\n", num, after);
+  int after = lineCnt(tmpTxt) + 1;  // get the line number after write
+  if (num == after) {
+    printf("Successfully wrote %d lines into %s.\n", num, tmpTxt);
+  } else {
+    printf("Failed: num = %d, after = %d\n", num, after);
   }
   printf("=========================\n");
-  #endif
+#endif
 
   /*
   2. Read words in tmp.txt and store in queue.
@@ -103,7 +107,7 @@ int main(int argc, char *argv[]) {
   printf("Storing words from %s to queue...\n", tmpTxt);
   queue_t *queue = createQ();
   FILE *readFile = fopen(tmpTxt, "r");
-  
+
   if (readFile == NULL) {
     perror("Error:");
     return 1;
@@ -117,103 +121,113 @@ int main(int argc, char *argv[]) {
     process_t *p = createProcess(cnt++, word, 0, 0);
     add2q(queue, p);
   }
-  
-  #if TEST
-    printQ(queue);
-    printf("==========Test2==========\n");
-    if(qsize(queue) == num){
-        printf("Successfully stored %d lines in queue from %s.\n", num, tmpTxt);
-    }
-    printf("=========================\n");
-  #endif
+
+#if TEST
+  printQ(queue);
+  printf("==========Test2==========\n");
+  if (qsize(queue) == num) {
+    printf("Successfully stored %d lines in queue from %s.\n", num, tmpTxt);
+  }
+  printf("=========================\n");
+#endif
 
   /*
   3. Encrypt 100 words a time through the subprocess of poolybius cipher.
   */
   printf("Encrypting words in subprocess...\n");
-  int pipefd[2];  // 0 for read, 1 for write
-  pid_t pid;
+  int num_batch = (num + BATCH_SIZE - 1) / BATCH_SIZE;
+  int pipefd[num_batch][2];  // 0 for read, 1 for write
+  pid_t pid[num_batch];
 
-  if (pipe(pipefd) == -1) {
-    perror("pipe");
-    exit(EXIT_FAILURE);
-  }
-
-  pid = fork();
-  if (pid == -1) {
-    perror("fork");
-    exit(EXIT_FAILURE);
-  }
-
-  if (pid == 0) {
-    // Child process
-    close(pipefd[1]);
-
-    int size = 6;
-    table_t *table = newTable(size);
-    char c = 'A';
-    for (int i = 0; i < table->size; i++) {
-      for (int j = 0; j < table->size; j++) {
-        table->square[i][j] = c++;
-      }
+  for (int i = 0; i < num_batch; i++) {
+    if (pipe(pipefd[i]) == -1) {
+      perror("pipe");
+      exit(EXIT_FAILURE);
     }
 
-    char buffer[SENTENCE_BUFFER_SIZE];
-    int wordCnt = 0;
-    while (read(pipefd[0], buffer, sizeof(buffer)) > 0) {
-      printf("Child process received: %s\n", buffer);
-    #if TEST
-      char temp[SENTENCE_BUFFER_SIZE];
-      strncpy(temp, buffer, sizeof(temp));
-      temp[sizeof(temp) - 1] = '\0';  // Ensure null termination
-      char *token = strtok(temp, " ");
-      while (token != NULL) {
-        wordCnt++;                    // Increment the word count
-        token = strtok(NULL, " ");  // Get the next token
-      }
-    #endif
-      writeCipherToFile(buffer, table);
+    pid[i] = fork();
+    if (pid[i] == -1) {
+      perror("fork");
+      exit(EXIT_FAILURE);
     }
-#if TEST
-      printf("==========Test3==========\n");
-        if(wordCnt == num){
-            printf("Successfully encrypted %d words.\n", num);
-        }else{
-            printf("Failed: wordCnt = %d\n", wordCnt);
+
+    if (pid[i] == 0) {
+      // Child process
+      close(pipefd[i][1]);
+
+      int size = 6;
+      table_t *table = newTable(size);
+      char c = 'A';
+      for (int i = 0; i < table->size; i++) {
+        for (int j = 0; j < table->size; j++) {
+          table->square[i][j] = c++;
         }
-      printf("=========================\n");
-    #endif
-
-
-    close(pipefd[0]);
-    freeTable(table);
-    exit(0);
-  } else {
-    close(pipefd[0]);
-
-    int cnt = 0;
-    char sentence[SENTENCE_BUFFER_SIZE] = "";
-
-    while (qsize(queue) > 0) {
-      process_t *p = (process_t *)popQ(queue);
-      strcat(sentence, p->name);
-      strcat(sentence, " ");
-      freeProcess(p);
-
-      if (++cnt % 100 == 0) {
-        ssize_t bytes_written = write(pipefd[1], sentence, strlen(sentence) + 1);
-        sleep(1);
-        sentence[0] = '\0';
       }
-    }
-    if (cnt % 100 != 0)
-      write(pipefd[1], sentence, strlen(sentence) + 1);  // Write data to pipe
 
-    close(pipefd[1]);
-    waitpid(pid, NULL, 0);  // Wait for child
+      char buffer[SENTENCE_BUFFER_SIZE];
+      int wordCnt = 0;
+      while (read(pipefd[i][0], buffer, sizeof(buffer)) > 0) {
+        printf("Child process %d received: %s\n", getpid(), buffer);
+#if TEST
+        char temp[SENTENCE_BUFFER_SIZE];
+        strncpy(temp, buffer, sizeof(temp));
+        temp[sizeof(temp) - 1] = '\0';  // Ensure null termination
+        char *token = strtok(temp, " ");
+        while (token != NULL) {
+          wordCnt++;                  // Increment the word count
+          token = strtok(NULL, " ");  // Get the next token
+        }
+#endif
+        writeCipherToFile(buffer, table);
+      }
+      close(pipefd[i][0]);
+      freeTable(table);
+      exit(wordCnt);
+    } else {
+      // Parent process
+      close(pipefd[i][0]);
+    }
   }
+
+  // Parent process
+  int cntWord = 0;
+  char sentence[SENTENCE_BUFFER_SIZE] = "";
+  int batch_idx = 0;
+
+  while (qsize(queue) > 0) {
+    process_t *p = (process_t *)popQ(queue);
+    strcat(sentence, p->name);
+    strcat(sentence, " ");
+    freeProcess(p);
+
+    if (++cntWord % BATCH_SIZE == 0 || qsize(queue) == 0) {
+      printf("Parent process sending to child process %d\n", pid[batch_idx]);
+      ssize_t bytes_written =
+          write(pipefd[batch_idx][1], sentence, strlen(sentence) + 1);
+      close(pipefd[batch_idx][1]);
+      sleep(1);
+      sentence[0] = '\0';
+      batch_idx++;
+    }
+  }
+
+  int totalWordCnt = 0;
+  cntWord = 0;
+  for (int i = 0; i < num_batch; i++) {
+    waitpid(pid[i], &cntWord, 0);
+    if(WIFEXITED(cntWord)){
+      totalWordCnt += WEXITSTATUS(cntWord);
+    }
+  }
+
+  #if TEST
+  printf("==========Test3==========\n");
+  if (totalWordCnt == num) {
+    printf("Successfully encrypted %d words in subprocess.\n", num);
+  }
+  #endif
 
   deleteQ(queue);
   fclose(readFile);
   return 0;
-} 
+}
